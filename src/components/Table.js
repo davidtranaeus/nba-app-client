52f4999cb362b39edc88537998d6a9cb1e9cef1b
davidtranaeus <davidtra@kth.se>
2019-11-10 16:56:52 +0100

    Send request to  api with axios and render a ul

---
+++ src/components/Table.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Table extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      standings: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3001/standings`)
+    .then(res => {
+      this.setState({
+        standings: res.data
+      })
+    }).catch(err => console.log(`Could not retrieve standings: ${err}`))
+  }
+
+  render() {
+    const standingsList = this.state.standings.map(standing => 
+      <li key={standing.teamId}>
+        {standing.teamId}
+      </li>)
+    return (
+      <ul>
+        { standingsList }
+      </ul>
+    )
+  }
+}
+
+export default Table;
