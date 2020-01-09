098e9e512679cefdf27cc352d86ea9e907763dc7
davidtranaeus <davidtra@kth.se>
2020-01-09 16:31:26 +0100

    Highlight the selected conference

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { VisibilityFilters } from '../actions'
 import './App.css';
 
-const Selector = ({ onClick }) => {
+const { SHOW_WEST, SHOW_EAST } = VisibilityFilters
+
+const Selector = ({ onClick, conference }) => {
   return (
     <div className="selector-container">
       {/* Lägg till markering på vad conference .selected/.unselected */}
       <div
-        onClick={() => onClick(VisibilityFilters.SHOW_WEST)}
+        onClick={() => onClick(SHOW_WEST)}
+        className={`${conference === SHOW_WEST ? "selected" : "unselected"}`}
         >Western conference
       </div>
       <div 
-        onClick={() => onClick(VisibilityFilters.SHOW_EAST)}
+        onClick={() => onClick(SHOW_EAST)}
+        className={`${conference === SHOW_EAST ? "selected" : "unselected"}`}
         >Eastern conference
       </div>
     </div>
--- src/containers/SelectorContainer.js
+++ src/containers/SelectorContainer.js
@@ -2,11 +2,15 @@ import { connect } from 'react-redux'
 import Selector from '../components/Selector'
 import { setVisibilityFilter } from '../actions'
 
+const mapStateToProps = state => ({
+  conference: state.visibilityFilter
+})
+
 const mapDispatchToProps = dispatch => ({
   onClick: (conference) => dispatch(setVisibilityFilter(conference))
 })
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Selector)
