11f56bda485c44cd9d461b7436b475abaf44b334
davidtranaeus <davidtra@kth.se>
2020-01-09 16:31:46 +0100

    Remove comment

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -7,7 +7,6 @@ const { SHOW_WEST, SHOW_EAST } = VisibilityFilters
 const Selector = ({ onClick, conference }) => {
   return (
     <div className="selector-container">
-      {/* Lägg till markering på vad conference .selected/.unselected */}
       <div
         onClick={() => onClick(SHOW_WEST)}
         className={`${conference === SHOW_WEST ? "selected" : "unselected"}`}
