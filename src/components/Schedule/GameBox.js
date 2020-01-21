b80f35361aa60d3d413d0176ab8af6a712ecdd56
David Tranaeus <dtranaeus@gmail.com>
2020-01-21 16:21:58 +0100

    Emphasize score of winning team

--- src/components/Schedule/GameBox.js
+++ src/components/Schedule/GameBox.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 const GameBox = ({ game, hTeam, vTeam }) => {
+
+  const hWon = parseInt(game.hTeam.score) > parseInt(game.vTeam.score)
+
   return (
     <li className="gamebox hover"> 
       <div>
         <img src={hTeam.logo} alt={hTeam.tricode} width="30px"/>
         <img src={vTeam.logo} alt={vTeam.tricode} width="30px"/>
       </div>
-      <div>{game.hTeam.score} - {game.vTeam.score}</div>
+      <div>
+        <span className={`${hWon ? "bold" : ""}`}>{game.hTeam.score} </span>
+        -
+        <span className={`${!hWon ? "bold" : ""}`}> {game.vTeam.score}</span>
+      </div>
       <div className="date">{parseUTCDate(game.startTimeUTC)}</div>
     </li>
   )
